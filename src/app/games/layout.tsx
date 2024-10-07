ca603a4768311d9ce4cc4c7fa1449a3224c5ffe6
Daniel Custom Code <danielmartins@customcode.com.br>
2024-10-07 16:27:12 -0300

    setup project with routes

---
+++ src/app/games/layout.tsx
@@ -0,0 +1,8 @@
+const GamesLayout = ({ children }: { children: React.ReactNode }) => {
+    return <div>
+        <p>Games Layout</p>
+        {children}
+    </div>
+}
+
+export default GamesLayout
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -24,10 +24,11 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
+    <html lang="pt">
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
+        <p>Oieeee</p>
         {children}
       </body>
     </html>
