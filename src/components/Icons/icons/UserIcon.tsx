4f4f0724d6ff2a556be6ab4c1b1194c832f2106c
Daniel Martins <danidiamoo1@gmail.com>
2024-10-08 15:22:48 -0300

    feature : adding icons and improve navbar

---
+++ src/components/Icons/icons/UserIcon.tsx
@@ -0,0 +1,9 @@
+//https://www.iconfinder.com/icons/1564534/customer_man_user_account_profile_icon
+export const UserIcon = ({ color = "currentColor", className = "" }) => {
+    return (
+        <svg enable-background="new 0 0 24 24" id="Layer_1" version="1.0" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg" className={className}>
+            <circle fill={color} cx="12" cy="8" r="4" />
+            <path fill={color} d="M12,14c-6.1,0-8,4-8,4v2h16v-2C20,18,18.1,14,12,14z" />
+        </svg>
+    )
+}
--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -1,16 +1,59 @@
+import {
+    GamepadIcon,
+    HomeIcon,
+    AwardIcon,
+    RouteIcon,
+    UserIcon,
+} from "../Icons/Icons";
+
+const NavBarList = ({ children, className }: { children: React.ReactNode, className?: string }) => {
+    return (
+        <ul className={`${className} my-4 border-t border-indigo-400/20 hover:border-indigo-400/40`}>
+            {children}
+        </ul>
+    );
+};
+
+const NavBarListItem = ({ children }: { children: React.ReactNode }) => {
+    return (
+        <li className="my-2 rounder-lg bg-transparent-900 px-2 hover:bg-indigo-400/40 cursor-pointer flex gap-2 items-center hover:text-slate-100">
+            {children}
+        </li>
+    );
+};
+
 export const Navbar = () => {
     return (
-        <nav className="flex flex-col bg-slate-900 border-r border-indigo-400/40 hover:border-indigo-400/80 w-72 p-2 h-screen">
-            <div><img src="https://emersonbroga.com/e/assets/emersonbroga-logo-name-pink.png" alt="Logo do site" className="max-w-full p-2" /></div>
-            <ul className="flex-grow my-4 border-t border-indigo-400/20 hover:border-indigo-400/40" >
-                <li className="my-2 rounder-lg bg-transparent-900 px-2 hover:bg-slate-800 cursor-pointer">Home</li>
-                <li className="my-2 rounder-lg bg-transparent-900 px-2 hover:bg-slate-800 cursor-pointer">Games</li>
-                <li className="my-2 rounder-lg bg-transparent-900 px-2 hover:bg-slate-800 cursor-pointer">Top 10</li>
-                <li className="my-2 rounder-lg bg-transparent-900 px-2 hover:bg-slate-800 cursor-pointer">Walkthroughs</li>
-            </ul>
-            <ul className="my-4 border-t border-indigo-400/20 hover:border-indigo-400/40" >
-                <li className="my-2 rounder-lg bg-transparent-900 px-2 hover:bg-slate-800 cursor-pointer">User</li>
-            </ul>
+        <nav className="flex flex-col bg-slate-900 border-r border-indigo-400/40 hover:border-indigo-400/80 w-72 p-2 h-screen text-slate-300">
+            <div className="flex items-center justify-center">
+                <img
+                    src="https://emersonbroga.com/e/assets/emersonbroga-logo-name-pink.png"
+                    alt="Logo do site"
+                    className="w-auto h-12 p-2"
+                />
+            </div>
+            <NavBarList className="flex-grow">
+                <NavBarListItem>
+                    <HomeIcon className="w-4 h-4" /> Home
+                </NavBarListItem>
+                <NavBarListItem>
+                    <GamepadIcon className="w-4 h-4" /> Games
+                </NavBarListItem>
+                <NavBarListItem>
+                    <AwardIcon className="w-4 h-4" />
+                    Top 10
+                </NavBarListItem>
+                <NavBarListItem>
+                    <RouteIcon className="w-4 h-4" /> Walkthroughs
+                </NavBarListItem>
+            </NavBarList>
+            <NavBarList>
+                <NavBarListItem>
+                    {" "}
+                    <UserIcon className="w-4 h-4" />
+                    User
+                </NavBarListItem>
+            </NavBarList>
         </nav>
-    )
-}
+    );
+};
